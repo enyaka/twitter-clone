55f26c7f0aa12280acb64191d0f021253ecc0225
Ensar Yasin Karaköse <ensaryasinkarakose@gmail.com>
2023-03-01 15:03:51 +0300

    Initialize

---
+++ TwitterClone2/Controller/MainTabController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabController.swift
+//  TwitterClone2
+//
+//  Created by Ensar Yasin Karaköse on 1.03.2023.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
